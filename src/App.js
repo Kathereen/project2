d3a2be3ae1d506f20b7668e3f64c5bb380640fb9
Kathereen <87031819+Kathereen@users.noreply.github.com>
2023-05-21 17:36:10 +0300

    Proj2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+      super(props)
+      this.state = {
+        items:[
+          {
+            id: 1,
+            name: 'Квартира-студія',
+            pict: '1-к квартира.jpg',
+            descr: 'Квартира однокімнатна. Дизайн студія - відкритий творчий простір.',
+            category: '1-кімнатні квартири',
+            price: '40000 $'
+
+          },
+          
+          {
+            id: 2,
+            name: 'Сімейна квартира',
+            pict: '2-к квартира.jpg',
+            descr: 'Квартира двокімнатна. Затишна сімейна квартира з дитячою кімнатою і великим балконом.',
+            category: '2-кімнатні квартири',
+            price: '50000 $'
+          },
+          {
+            id: 3,
+            name: 'Апартаменти',
+            pict: 'дворівнева квартира.jpg',
+            descr: 'Просторі апартаменти для самого вибагливого власника',
+            category: '2-кімнатні квартири',
+            price: '60000 $'
+          }
+        ]
+      }
+      
+    }
+  render(){
   return (
     <div className="wrapper">
       Нерухомість
@@ -9,6 +45,7 @@ function App() {
       <Footer/>
     </div>
   );
+}
 }
 
 export default App;
